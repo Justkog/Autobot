0f7545b1fc780b53889d0ef4152a247fbcc348f1
Labelec-vm <Labelec-vm>
2016-03-22 17:01:53 +0100

    TP firmware start

---
+++ AUTOBOTv2 A LA PLAGE.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=AUTOBOTv2_A_LA_PLAGE.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/AUTOBOTv2_A_LA_PLAGE.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=autobotv2alaplage.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/autobotv2alaplage.x.tar
---
+++ Autobot Firmware TP.X/main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: Autobots
+ *
+ * Created on March 22, 2016, 3:42 PM
+ */
+
+#include <p32xxxx.h>
+#include "types.h"
+
+int main(void)
+{
+    LATF |= 0x2 ;
+    TRISF |= 0x2;
+    while (1)
+    {
+        if ()
+            LATF |= 0x2 ;
+    }
+    return (0);
+}
